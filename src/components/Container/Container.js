e26bd4665450675344167d187ea129239908e31c
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-16 00:07:17 +0200

    Container c-t refactoring

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import s from './Container.module.css';
 
-class Container extends Component {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-  };
-
-  render() {
-    const { children } = this.props;
-    return <div className={s.container}>{children}</div>;
-  }
+export default function Container({ children }) {
+  return <div className={s.container}>{children}</div>;
 }
 
-export default Container;
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
