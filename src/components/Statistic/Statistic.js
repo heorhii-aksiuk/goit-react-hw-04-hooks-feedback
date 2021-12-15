1279419b246b68da184f06324f78e45177d79f57
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-16 00:32:42 +0200

    Statistic c-t refactoring

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import s from './Statistic.module.css';
 
-class Statistic extends Component {
-  static propTypes = {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired,
-  };
-
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
-    return (
-      <ul className={s.list}>
-        <li className={s.item}>Good: {good}</li>
-        <li className={s.item}>Neutral: {neutral}</li>
-        <li className={s.item}>Bad: {bad}</li>
-        <li className={s.item}>Total: {total}</li>
-        <li className={s.item}>Positive feedback: {positivePercentage}%</li>
-      </ul>
-    );
-  }
+export default function Statistic({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) {
+  return (
+    <ul className={s.list}>
+      <li className={s.item}>Good: {good}</li>
+      <li className={s.item}>Neutral: {neutral}</li>
+      <li className={s.item}>Bad: {bad}</li>
+      <li className={s.item}>Total: {total}</li>
+      <li className={s.item}>Positive feedback: {positivePercentage}%</li>
+    </ul>
+  );
 }
 
-export default Statistic;
+Statistic.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
