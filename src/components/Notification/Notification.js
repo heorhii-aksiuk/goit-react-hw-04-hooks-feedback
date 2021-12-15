81cf2c1017874cc09dc19beec5e91e5c5fd7bf45
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-16 00:28:12 +0200

    Notification c-t refactoring

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import s from './Notification.module.css';
 
-class Notification extends Component {
-  static propTypes = {
-    message: PropTypes.string.isRequired,
-  };
-
-  render() {
-    const { message } = this.props;
-    return <p className={s.notifaction}>{message}</p>;
-  }
+export default function Notification({ message }) {
+  return <p className={s.notifaction}>{message}</p>;
 }
 
-export default Notification;
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
