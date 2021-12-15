36aacb86c7247fe39568d8644ba59f76d13398ae
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-16 00:30:28 +0200

    Section c-t refactoring

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,23 +1,16 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import s from './Section.module.css';
 
-class Section extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
-  };
-
-  render() {
-    const { title, children } = this.props;
-    return (
-      <section className={s.section}>
-        <h2 className={s.title}>{title}</h2>
-        {children}
-      </section>
-    );
-  }
+export default function Section({ title, children }) {
+  return (
+    <section className={s.section}>
+      <h2 className={s.title}>{title}</h2>
+      {children}
+    </section>
+  );
 }
 
-export default Section;
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
